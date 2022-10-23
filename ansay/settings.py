1e997b701272700c661d3e2531ba2e1c2f542bac
spicke23 <angelo.valenzuela@alumnos.uta.cl>
2022-10-22 23:17:37 -0300

    Modificacion de settings.py
    Por problemas de "secretos" se modifica el SECRET_KEY
    OCTUBRE.22.22

--- ansay/settings.py
+++ ansay/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o37hwoq-g@f^wful2&3@x_%oc6xt&e5apq4q-poi+-6(k061(2'
+SECRET_KEY = '******************************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
